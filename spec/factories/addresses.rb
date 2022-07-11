c41334c9d6f55b28574d9b45da8f25ae1052bf63
Vorachith Kip <songta17@hotmail.com>
2022-07-12 01:39:11 +0200

    fix spec

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,4 +1,5 @@
 FactoryBot.define do
+  LIST_ZIPCODE = (75001..75020).to_a
   factory :address do
     sequence(:street) { |n| "#{Faker::Address.street_name} #{n}" }
     sequence(:zipcode) { |n| LIST_ZIPCODE.sample }
