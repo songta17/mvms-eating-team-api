a0b81e5c5f0457f4d021a9417be30cc0704d56f7
Vorachith Kip <songta17@hotmail.com>
2022-07-12 05:18:13 +0200

    findus fix

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,8 +1,9 @@
+List = [75001, 75002, 75003, 75004, 75005, 75006, 75007, 75008, 75009, 75010, 75011, 75012, 75013, 75014, 75015, 75016, 75017, 75018, 75019, 75020]
+
 FactoryBot.define do
-  LIST_ZIPCODE = (75001..75020).to_a
   factory :address do
     sequence(:street) { |n| "#{Faker::Address.street_name} #{n}" }
-    sequence(:zipcode) { |n| LIST_ZIPCODE.sample }
+    sequence(:zipcode) { |n| List.sample }
     town { "Paris" }
     country { "France" }
     association :restaurant
